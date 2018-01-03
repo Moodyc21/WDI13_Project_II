3ee9d6ed81bd34aa9f1ac1b75d15c29973783987
Christopher Moody <moodyc21@gmail.com>
2018-01-03 18:45:47 -0500

    Updated DB connection code to use MongoLab when in Production

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+require('dotenv').config()
+
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+
 
 const index = require('./routes/index');
 const users = require('./routes/users');
@@ -24,6 +28,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+
+//connect MongoDB
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/completeBoard');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
