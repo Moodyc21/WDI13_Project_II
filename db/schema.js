175ff130d1eee317e65a6a4e7bdf307dd94f0a4d
Christopher Moody <moodyc21@gmail.com>
2018-01-06 17:40:20 -0500

    Created and fixed user schema to seed users

--- db/schema.js
+++ db/schema.js
@@ -3,143 +3,226 @@ const Schema = mongoose.Schema
 
 mongoose.Promise = global.Promise
 
-const DeckSchema = new Schema(
-    {
-        company: {
-            type: String,
-            required: [true, 'Deck company is required!']
-        },
-        size: {
-            type: Number
-        },
-        shape: {
+// const DeckSchema = new Schema(
+//     {
+//         company: {
+//             type: String,
+//             required: [true, 'Deck company is required!']
+//         },
+//         size: {
+//             type: Number
+//         },
+//         shape: {
+//             type: String
+//         },
+
+//     },
+//     {
+//         timestamps: {}
+//     }
+// )
+
+// const TruckSchema = new Schema(
+//     {
+//         company: {
+//             type: String,
+//             required: [true, 'Deck company is required!']
+//         },
+//         size: {
+//             type: Number
+//         },
+
+//     },
+//     {
+//         timestamps: {}
+//     }
+// )
+
+// const WheelSchema = new Schema(
+//     {
+//         company: {
+//             type: String,
+//             required: [true, 'Deck company is required!']
+//         },
+//         size: {
+//             type: Number
+//         },
+
+//     },
+//     {
+//         timestamps: {}
+//     }
+// )
+
+// const BearingSchema = new Schema(
+//     {
+//         company: {
+//             type: String,
+//             required: [true, 'Deck company is required!']
+//         },
+//         Abec: {
+//             type: Number
+//         },
+
+//     },
+//     {
+//         timestamps: {}
+//     }
+// )
+
+// const GripTapeSchema = new Schema(
+//     {
+//         company: {
+//             type: String,
+//             required: [true, 'Deck company is required!']
+//         },
+//         grit: {
+//             type: Number
+//         },
+
+//     },
+//     {
+//         timestamps: {}
+//     }
+// )
+
+// const HardwareSchema = new Schema(
+//     {
+//         company: {
+//             type: String,
+//             required: [true, 'Deck company is required!']
+//         },
+//         size: {
+//             type: Number
+//         },
+
+//     },
+//     {
+//         timestamps: {}
+//     }
+// )
+
+// const CompleteSchema = new Schema(
+//     {
+//         name: String
+//     },
+//     {
+//         deck: DeckSchema
+//     },
+//     {
+//         trucks: TruckSchema
+//     },
+//     {
+//         wheels: WheelSchema
+//     },
+//     {
+//         bearings: BearingSchema
+//     },
+//     {
+//         gripTape: GripTapeSchema
+//     },
+//     {
+//         hardware: HardwareSchema
+//     },
+
+
+//     {
+//         timestamps: {},
+//     }
+// )
+
+// const UserSchema = new Schema(
+//     {
+//         name: {
+//             type: String,
+//             required: [true, 'Name is required!']
+//         },
+//         stance: {
+//             type: String,
+//             required: [true, 'Stance is required!']
+//         },
+//         photo: {
+//             type: String,
+//         },
+//         myBoard: CompleteSchema
+//     },
+//     {
+//         timestamps: {},
+//         usePushEach: true
+//     }
+// )
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required!']
+    },
+    stance: {
+        type: String,
+        required: [true, 'Stance is required!']
+    },
+    photo: {
+        type: String
+    },
+    complete: [{
+        name: {
             type: String
         },
-
-    },
-    {
-        timestamps: {}
-    }
-)
-
-const TruckSchema = new Schema(
-    {
-        company: {
-            type: String,
-            required: [true, 'Deck company is required!']
-        },
-        size: {
-            type: Number
-        },
-
-    },
-    {
-        timestamps: {}
-    }
-)
-
-const WheelSchema = new Schema(
-    {
-        company: {
-            type: String,
-            required: [true, 'Deck company is required!']
-        },
-        size: {
-            type: Number
-        },
-
-    },
-    {
-        timestamps: {}
-    }
-)
-
-const BearingSchema = new Schema(
-    {
-        company: {
-            type: String,
-            required: [true, 'Deck company is required!']
-        },
-        Abec: {
-            type: Number
-        },
-
-    },
-    {
-        timestamps: {}
-    }
-)
-
-const GripTapeSchema = new Schema(
-    {
-        company: {
-            type: String,
-            required: [true, 'Deck company is required!']
-        },
-        grit: {
-            type: Number
-        },
-
-    },
-    {
-        timestamps: {}
-    }
-)
-
-const HardwareSchema = new Schema(
-    {
-        company: {
-            type: String,
-            required: [true, 'Deck company is required!']
-        },
-        size: {
-            type: Number
-        },
-
-    },
-    {
-        timestamps: {}
-    }
-)
-
-const CompleteSchema = new Schema(
-    {
-        deck: DeckSchema
-    },
-    {
-        trucks: TruckSchema
-    },
-    {
-        wheels: WheelSchema
-    },
-    {
-        bearings: BearingSchema
-    },
-    {
-        gripTape: GripTapeSchema
-    },
-    {
-        hardware: HardwareSchema
-    },
+        deck: [{
+            company: {
+                type: String
+            },
+            size: {
+                type: Number
+            },
+            shape: {
+                type: String
+            }
+        }],
+        trucks: [{
+            company: {
+                type: String
+            },
+            size: {
+                type: Number
+            }
+        }],
+        wheels: [{
+            company: {
+                type: String
+            },
+            size: {
+                type: Number
+            }
+        }],
+        bearings: [{
+            company: {
+                type: String
+            },
+            abec: {
+                type: Number
+            }
+        }],
+        gripTape: [{
+            company: {
+                type: String
+            },
+            grit: {
+                type: Number
+            }
+        }],
+        hardware: [{
+            company: {
+                type: String
+            },
+            size: {
+                type: Number
+            }
+        }]
 
 
-    {
-        timestamps: {}
-    }
-)
-
-const UserSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: [true, 'Name is required!']
-        },
-        stance: {
-            type: String,
-            required: [true, 'Stance is required!']
-        },
-        myBoard: CompleteSchema
-    },
+    }]
+},
     {
         timestamps: {},
         usePushEach: true
@@ -147,12 +230,12 @@ const UserSchema = new Schema(
 )
 
 module.exports = {
-    DeckSchema,
-    TruckSchema,
-    WheelSchema, 
-    BearingSchema, 
-    GripTapeSchema, 
-    HardwareSchema, 
-    CompleteSchema,
+    // DeckSchema,
+    // TruckSchema,
+    // WheelSchema,
+    // BearingSchema,
+    // GripTapeSchema,
+    // HardwareSchema,
+    // CompleteSchema,
     UserSchema
 }
--- db/seeds.js
+++ db/seeds.js
@@ -1,47 +1,119 @@
 require('dotenv').config()
 const User = require('./models/User')
-const Complete = require('./models/Complete')
-const Deck = require('./models/Deck')
+// const Complete = require('./models/Complete')
+// const Deck = require('./models/Deck.js')
+// const Trucks = require('./models/Truck')
+// const Wheels = require('./models/Wheel')
+// const Bearings = require('./models/Bearing')
+// const GripTape = require('./models/Griptape')
+// const Hardware = require('./models/Hardware')
 const mongoose = require('mongoose')
 
 // connect to database
-mongoose.connect(process.env.MONGODB_URI, {
-  useMongoClient: true
-})
+mongoose.connect(process.env.MONGODB_URI)
 
 mongoose.connection.once('open', () => {
-  console.log(`Mongoose has connected to MongoDB`)
+    console.log(`Mongoose has connected to MongoDB`)
 })
 
 mongoose.connection.on('error', (error) => {
-  console.error(`
+    console.error(`
     MongoDB connection error!!! 
     ${error}
   `)
-  process.exit(-1)
+    process.exit(-1)
 })
 
 // Delete all users, then add some fake ones
 User.remove({}).then(() => {
-  const bobby = new User({
-    name: 'Bob Murray',
-    stance: 'goofy',
-  })
 
+    // const bobsDeck = new Deck({
+    //     company: 'Real',
+    //     size: 8.0,
+    //     shape: 'mConcave'
+    // })
 
-  return bobby.save()
-}).then(() => {
-  return User.create({
-    name: 'Brian Kubis',
-    stance: 'regular',
-  })
+    // const bobsTrucks = new Trucks({
+    //     company: 'Royal',
+    //     size: 139
+    // })
+    // const bobsWheels = new Wheels({
+    //     company: 'Bones',
+    //     size: 49
+    // })
+    // const bobsBearings = new Bearings({
+    //     company: 'Boneswiss',
+    //     abec: 7
+    // })
+    // const bobsGrip = new GripTape({
+    //     company: 'MOB',
+    //     grit: 800
+    // })
+    // const bobsHardware = new Hardware({
+    //     company: 'Lucky',
+    //     size: 1
+    // })
+    // const bobsCom = new Complete({
+    //     name: 'bobsBoard',
+    //     Deck: bobsDeck,
+    //     Trucks: bobsTrucks,
+    //     Wheels: bobsWheels,
+    //     Bearings: bobsBearings,
+    //     GripTape: bobsGrip,
+    //     Hardware: bobsHardware
+
+    // })
+
+    // const bobby = new User({
+    //     name: 'Bob Murray',
+    //     stance: 'goofy',
+    //     photo: '',
+    //     myBoard: bobsCom
+    // })
+
+    const bobby = new User({
+        name: 'Bob Murray',
+        stance: 'goofy',
+        photo: '',
+        complete: [{
+            name: 'bobsBoard',
+            deck: [{
+                company: 'Real',
+                size: 8.0,
+                shape: 'mConcave'
+            }],
+            trucks: [{
+                company: 'Royal',
+                size: 139
+            }],
+            wheels: [{
+                company: 'Bones',
+                size: 49
+            }],
+            bearings: [{
+                company: 'Boneswiss',
+                abec: 7
+            }],
+            grip: [{
+                company: 'MOB',
+                grit: 800
+            }],
+            hardware: [{
+                company: 'Lucky',
+                size: 1
+            }]
+
+        }]
+    })
+
+    return bobby.save()
 
 }).catch((error) => {
-  console.log('!!!!! ERROR SAVING SEEDED DATA !!!!!')
-  console.log(error)
+    console.log('!!!!! ERROR SAVING SEEDED DATA !!!!!')
+    console.log(error)
 }).then(() => {
-  mongoose.connection.close()
-  console.log(`
+    mongoose.connection.close()
+    console.log(`
       Finished seeding database...
       
       Disconnected from MongoDB
